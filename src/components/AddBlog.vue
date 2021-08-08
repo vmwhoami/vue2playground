1dcb3f36ee9a2e05515a68f44e711420b3ca2486
vmwhoami <vmwhoami@gmail.com>
2021-08-08 21:15:44 +0300

    Learn to add checkboxes and input fields

--- src/components/AddBlog.vue
+++ src/components/AddBlog.vue
@@ -6,17 +6,33 @@
     type="text"
      name="title"
       id="title"
-      v-model="formData.title"
+      v-model.lazy="formData.title"
     >
+
     <label for="text_field">Content</label>
-    <textarea type="text" name="text_field" id="text_field"
-    v-model="formData.textField"
-    />
+    <textarea class="rounded" type="text" name="text_field" id="text_field"
+    v-model.lazy="formData.textField"
+    ></textarea>
   </form>
+  <div class="checkboxes">
+    <label for="vitalie"> Vitalie</label>
+        <input v-model="formData.checkbox" value="vitalie" type="checkbox" id="vitalie" class="rounded text-pink-500" />
+    <label for="ninjas"> Ninjas</label>
+           <input v-model="formData.checkbox" value="ninjas" type="checkbox" id="ninjas" class="rounded text-pink-500" />
+    <label for="olea"> Olea</label>
+      <input v-model="formData.checkbox" value="olea" type="checkbox" id="olea" class="rounded text-pink-500" />
+  </div>
   <div class="content">
     <p>{{formData.title}}</p>
     <p>{{formData.textField}}</p>
-
+    <h2>
+      CheckBox values:
+      <ul>
+        <li  v-for="element in formData.checkbox" :key="element">
+          {{ element }}
+        </li>
+      </ul>
+    </h2>
   </div>
   </div>
 </template>
@@ -27,7 +43,8 @@
     return{
       formData:{
         title:"",
-        textField:""
+        textField:"",
+        checkbox:[]
       }
     }
   }
