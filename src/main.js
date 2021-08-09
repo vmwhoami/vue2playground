33665dec407e732b3c40331b6e0d952c8d5b8c88
Vitalie Melnic <vitalie.m@travod.com>
2021-08-09 21:40:13 +0300

    Install vue-resource

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue'
 import './assets/tailwind.css';
+import VueResource from 'vue-resource';
 
 Vue.config.productionTip = false
+Vue.use(VueResource);
 
 export default new Vue();
 
