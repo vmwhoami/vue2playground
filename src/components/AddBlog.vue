785d8abd693db525e16b22a3286218e577ce6507
Vitalie Melnic <vitalie.m@travod.com>
2021-08-09 22:23:13 +0300

    Make api call to my-json-server

--- src/components/AddBlog.vue
+++ src/components/AddBlog.vue
@@ -6,7 +6,7 @@
       type="text"
       name="title"
       id="title"
-      v-model.lazy="formData.title">
+      v-model.lazy="blog.title">
 
       <label for="text_field">Content</label>
       <textarea 
@@ -14,35 +14,41 @@
       type="text" 
       name="text_field" 
       id="text_field"
-      v-model.lazy="formData.textField"
+      v-model.lazy="blog.textField"
       ></textarea>
 
     <div class="checkboxes">
       <label for="vitalie"> Vitalie</label>
-      <input v-model="formData.checkbox" value="vitalie" type="checkbox" id="vitalie" class="rounded text-pink-500" />
+      <input v-model="blog.checkbox" value="vitalie" type="checkbox" id="vitalie" class="rounded text-pink-500" />
       <label for="ninjas"> Ninjas</label>
-      <input v-model="formData.checkbox" value="ninjas" type="checkbox" id="ninjas" class="rounded text-pink-500" />
+      <input v-model="blog.checkbox" value="ninjas" type="checkbox" id="ninjas" class="rounded text-pink-500" />
       <label for="olea"> Olea</label>
-      <input v-model="formData.checkbox" value="olea" type="checkbox" id="olea" class="rounded text-pink-500" />
+      <input v-model="blog.checkbox" value="olea" type="checkbox" id="olea" class="rounded text-pink-500" />
     </div>
 
     <div class="selected mr-20">
-      <select v-model="formData.author"> 
+      <select v-model="blog.author"> 
         <option v-for="author in authors" :key="author">{{author}}</option>
       </select>
     </div>
+    <button 
+    class="bg-red-200 px-4 py-2 rounded hover:bg-purple-300" 
+    @click.prevent="submitForm" 
+    type="submit">
+      Submit
+    </button>
   </form>
 
     <div class="content">
-      <p>{{formData.title}}</p>
-      <p>{{formData.textField}}</p>
+      <p>{{blog.title}}</p>
+      <p>{{blog.textField}}</p>
       <h2>CheckBox values:</h2>
         <ul>
-          <li  v-for="element in formData.checkbox" :key="element">
+          <li  v-for="element in blog.checkbox" :key="element">
             {{ element }}
           </li>
         </ul>
-        <p>Author: {{formData.author}}</p>
+        <p>Author: {{blog.author}}</p>
     </div>
   </div>
 </template>
@@ -51,13 +57,24 @@
  export default{
   data(){
     return{
-      formData:{
+      blog:{
         title:"",
         textField:"",
         checkbox:[],
         author: ""
       },
       authors:["Hemingway","Tolstoy","Artos","Bartos"],
+    }
+  },
+  methods:{
+    submitForm(){
+      this.$http.post("http://my-json-server.typicode.com/posts",{
+        title:this.blog.title,
+        body:this.blog.textField,
+        userId:1,
+      }).then((data)=>{
+        console.log(data);
+      }).catch((err)=>console.log(err))
     }
   }
  }
