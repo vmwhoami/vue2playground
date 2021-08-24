10fe1982e52568adc0e5b16832c70dd1aafa5e56
Vitalie Melnic <vitalie.m@travod.com>
2021-08-24 22:10:32 +0300

    Learn how to get a single element and display it

---
+++ src/components/SingleBlog.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="single-blog">
+      <h1></h1>
+      <article></article>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            id:this.$route.params.id,
+            blog:{}
+        }
+    },
+    created(){
+        //This wont work cause this route doesn't work like that
+        this.$http.get(`https://api.publicapis.org/entries/${this.id}`)
+    }
+}
+</script>
+
+<style>
+
+</style>
