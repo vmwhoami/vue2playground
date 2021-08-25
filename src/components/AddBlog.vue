603772af5df15e339a56f610156d64cc940045a2
Vitalie Melnic <vitalie.m@travod.com>
2021-08-25 21:38:04 +0300

    Add tailwind styling to the addBlog area

--- src/components/AddBlog.vue
+++ src/components/AddBlog.vue
@@ -1,86 +1,80 @@
 <template>
-  <div class="form_container">
-    <form class="form flex-column">
-      <label for="title">Title</label>
-      <input 
-      type="text"
-      name="title"
-      id="title"
-      v-model.lazy="blog.title">
-
-      <label for="text_field">Content</label>
-      <textarea 
-      class="rounded"
-      type="text" 
-      name="text_field" 
-      id="text_field"
-      v-model.lazy="blog.textField"
-      ></textarea>
-
-    <div class="checkboxes">
-      <label for="vitalie"> Vitalie</label>
-      <input v-model="blog.checkbox" value="vitalie" type="checkbox" id="vitalie" class="rounded text-pink-500" />
-      <label for="ninjas"> Ninjas</label>
-      <input v-model="blog.checkbox" value="ninjas" type="checkbox" id="ninjas" class="rounded text-pink-500" />
-      <label for="olea"> Olea</label>
-      <input v-model="blog.checkbox" value="olea" type="checkbox" id="olea" class="rounded text-pink-500" />
-    </div>
-
-    <div class="selected mr-20">
-      <select v-model="blog.author"> 
-        <option v-for="author in authors" :key="author">{{author}}</option>
-      </select>
-    </div>
-    <button 
-    class="bg-red-200 px-4 py-2 rounded hover:bg-purple-300" 
-    @click.prevent="submitForm" 
-    type="submit">
-      Submit
-    </button>
-  </form>
-
-    <div class="content">
-      <p>{{blog.title}}</p>
-      <p>{{blog.textField}}</p>
-      <h2>CheckBox values:</h2>
-        <ul>
-          <li  v-for="element in blog.checkbox" :key="element">
-            {{ element }}
-          </li>
-        </ul>
-        <p>Author: {{blog.author}}</p>
+  <div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-6 bg-white border-b border-gray-200">
+          <form method="POST">
+            <div class="mb-4">
+              <label class="text-xl text-gray-600"
+                >Title <span class="text-red-500">*</span></label
+              >
+              <input
+                type="text"
+                name="title"
+                class="border-2 border-gray-300 p-2 w-full"
+                id="title"
+                v-model.lazy="blog.title"
+              />
+            </div>
+            <div class="mb-8">
+              <label class="text-xl text-gray-600"
+                >Content <span class="text-red-500">*</span></label
+              >
+              <textarea
+                class="border-2 border-gray-500"
+                type="text"
+                name="text_field"
+                id="text_field"
+                v-model.lazy="blog.textField"
+              ></textarea>
+            </div>
+            <div class="flex p-1">
+              <button
+                class="p-3 bg-blue-500 text-white hover:bg-blue-400"
+                @click.prevent="submitForm"
+                type="submit"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
- export default{
-  data(){
-    return{
-      blog:{
-        title:"",
-        textField:"",
-        checkbox:[],
-        author: ""
+export default {
+  data() {
+    return {
+      blog: {
+        title: "",
+        textField: "",
+        checkbox: [],
+        author: "",
       },
-      authors:["Hemingway","Tolstoy","Artos","Bartos"],
-      submitted: false
-    }
+      authors: ["Hemingway", "Tolstoy", "Artos", "Bartos"],
+      submitted: false,
+    };
   },
-  methods:{
-    submitForm(){
+  methods: {
+    submitForm() {
       console.log(this.blog);
-      this.$http.post("https://vuejs-18224-default-rtdb.europe-west1.firebasedatabase.app/posts.json", this.blog)
-      .then((data)=>{
-        this.submitted = true
-       console.log(data)
-        }).catch((err)=>console.log(err))
-    }
+      this.$http
+        .post(
+          "https://vuejs-18224-default-rtdb.europe-west1.firebasedatabase.app/posts.json",
+          this.blog
+        )
+        .then((data) => {
+          this.submitted = true;
+          console.log(data);
+        })
+        .catch((err) => console.log(err));
+    },
   },
-
- }
+};
 </script>
 
 <style scoped>
-
 </style>
