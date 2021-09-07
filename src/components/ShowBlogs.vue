0c180adce059d1b4dc4895b80377c1b1b30e1744
Vitalie Melnic <vitalie.m@travod.com>
2021-09-07 22:11:01 +0300

    Redizign main showBlogs page

--- src/components/ShowBlogs.vue
+++ src/components/ShowBlogs.vue
@@ -1,73 +1,90 @@
 <template>
- <div>
-     <h2 >Show blogs</h2>
-     <input type="text" v-model="searchField" placeholder="SearchBox" > 
-     <li v-theme ="'wide'" v-rainbow v-for="post in filteredBlogs" :key="post.title">
-       <h2>
-         <router-link :to="'blog/'+post.id">
-         {{post.title | to-uppercase}}
-         </router-link>
-         </h2>
-       <div v-html="post.textField"></div>
-       <span>{{post.timeStamp}}</span>
-     </li>
- </div>
+  <div>
+    <h2>Show blogs</h2>
+    <input
+      class="mx-7 lg:mx-6 mt-32 flex-grow"
+      type="text"
+      v-model="searchField"
+      placeholder="SearchBox"
+    />
+    <main class="mx-7 lg:mx-6 mt-32 flex-grow">
+      <div class="flex flex-wrap -mx-2">
+        <div
+          class="w-full sm:w-1/2 md:w-1/3 self-stretch p-2 mb-2"
+          v-for="post in filteredBlogs"
+          :key="post.title"
+        >
+              <router-link :to="'blog/' + post.id">
+          <div class="rounded shadow-md h-full">
+            <h2>
+                {{ post.title}}
+            </h2>
+            <div v-html="post.textField"></div>
+            <span>{{ post.timeStamp }}</span>
+            
+          </div>
+            </router-link>
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
 
+
 <script>
-import searchMixin from '../mixins/searchMain';
-import purifyText from '../mixins/purifyText';
-import {mapState} from 'vuex';
- 
- export default{
-  data(){
-    return{
-        posts:[],
-        searchField:'',
-    }
- },
-  created(){
+import searchMixin from "../mixins/searchMain";
+import purifyText from "../mixins/purifyText";
+import { mapState } from "vuex";
+
+export default {
+  data() {
+    return {
+      posts: [],
+      searchField: "",
+    };
+  },
+  created() {
     this.$http
       .get(
         "https://vuejs-18224-default-rtdb.europe-west1.firebasedatabase.app/posts.json"
-      ).then(data=>{
-        return  data.json()})
-        .then(data=>{
-          const arrayOfObjects = []
-          for (let key in data) {
-            data[key].id = key
-            arrayOfObjects.push(data[key])
-          }
-         this.posts = arrayOfObjects;
-         console.log(arrayOfObjects);
-        })
-  }, 
-   filters:{
-     toUppercase(value){
+      )
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        const arrayOfObjects = [];
+        for (let key in data) {
+          data[key].id = key;
+          arrayOfObjects.push(data[key]);
+        }
+        this.posts = arrayOfObjects;
+        console.log(arrayOfObjects);
+      });
+  },
+  filters: {
+    toUppercase(value) {
       return value.toUpperCase();
-     }
-   },
-   directives:{
-     'theme':{
-        bind: (el, binding)=>{
-        if(binding.value == "wide"){
+    },
+  },
+  directives: {
+    theme: {
+      bind: (el, binding) => {
+        if (binding.value == "wide") {
           el.style.maxWidth = "800px";
           el.style.margin = "0 auto";
-          el.style.padding = "10px 20px"
+          el.style.padding = "10px 20px";
         }
-      }
-     }
-   },
-   mixins:[searchMixin,purifyText],
-   computed: mapState({
-       someName:state =>state.propertiesName
-     })
-     
- }
+      },
+    },
+  },
+  mixins: [searchMixin, purifyText],
+  computed: mapState({
+    someName: (state) => state.propertiesName,
+  }),
+};
 </script>
 .red
 <style scoped>
-
 </style>
 
 
